fbc0a88e1c873efd561850c1f16ecc611e1dd938
Kim Biesbjerg <kb@mindly.dk>
2019-07-03 16:08:25 +0200

    add modal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    protected modalController: ModalController
+  ) {}
+
+    async openModal() {
+        const modal = await this.modalController.create({
+            component: HomePage
+        });
+        return await modal.present();
+    }
 
 }
